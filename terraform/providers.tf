6677b2b98e267f246954dbb485305b23e62aa6d4
Melvyn de Kort <melvyn@mdekort.nl>
2023-05-19 13:43:32 +0200

    Migrate state to Terraform Cloud

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+    organization = "melvyndekort"
+
+    workspaces {
+      name = "cheatsheets"
+    }
+  }
+
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
